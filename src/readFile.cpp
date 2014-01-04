f6fb79d025898c390011bf57434378ee7ae5bf81
R0mai <r0maikx02b@gmail.com>
2014-01-04 21:36:32 +0100

    Added readFile.?pp

---
+++ src/readFile.cpp
@@ -0,0 +1,15 @@
+
+#include "readFile.hpp"
+
+#include <fstream>
+
+std::string readFile(const std::string& fileName) {
+	std::ifstream in(fileName);
+	std::string result;
+	std::string line;
+	while (std::getline(in, line)) {
+			result += line + '\n';
+		}
+	return result;
+}
+
---
+++ src/readFile.hpp
@@ -0,0 +1,10 @@
+
+#ifndef READFILE_HPP_
+#define READFILE_HPP_
+
+#include <string>
+
+std::string readFile(const std::string& fileName);
+
+#endif
+
