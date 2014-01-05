2cbb679bf91485b97317b178f068f517771cac9a
R0mai <r0maikx02b@gmail.com>
2014-01-05 01:29:42 +0100

    Delete files when done

--- src/gatherRankings.sh
+++ src/gatherRankings.sh
@@ -6,7 +6,13 @@ aggregatedFile="tmp/aggregatedFile"
 
 for playerIndex in {1..10}; do
 	tmpFile="tmp/tmp"
+
 	src/downloadPage.sh "95.130.9.140/TM/Trophee2014/Resultats_hiver/statistiques_joueur.php?joueur=${playerIndex}" ${tmpFile}
+
 	awk -f src/parseRankPage.awk -v player=${playerIndex} ${tmpFile} >> "${aggregatedFile}"
+
+	rm -f ${tmpFile}
 done
+
+rm -f ${aggregatedFile}
 
